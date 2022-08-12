494148a713b7c89180830af7941f545e994caf8c
Rick Hdz <rickhdz@Ricks-MacBook-Pro.local>
2022-08-12 14:54:20 +0200

    Endpoint request connected and showing info, TODO: Styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const main_url = "http://localhost:3000";
+  const [commits, setCommits] = useState([]);
+
+
+  const getCommits = () => {
+    axios
+       .get(main_url + "/api/v1/github/get_commits/tuxrick/FTF_test_api")
+       .then((resp) => {
+         setCommits(resp.data.data);
+       });
+  };
+
+  useState(() => {
+    getCommits();
+  }, []);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+        {
+          /*
+            <img src={logo} className="App-logo" alt="logo" />
+          */
+        }
+        
+        {commits.map((commit, index) =>
+          <div key={index}>
+            <p>{commit.commit.author.name}</p>
+            <p>{commit.commit.author.email}</p>
+            <p>{commit.commit.message}</p>
+          </div>
+        )}
+
+        {
+          /*
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>          
+          */
+        }
     </div>
   );
 }
